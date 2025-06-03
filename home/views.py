1be8f7552d0df8b2a69aa630cd23f6922f7329b4
Nirmal Kumar <aman.kumar@ncfe.ac.in>
2025-06-03 12:27:40 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+
+#home
+def home(request):
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
